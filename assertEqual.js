eb6dd58941a353e2aad685d621f1c27a037101d7
zezking <zhaoenze001@gmail.com>
2021-02-01 22:18:03 +0000

    update with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -2,19 +2,25 @@ const emoji = require('node-emoji');
 const { log } = console;
 const pizza = emoji.get('pizza')
 const tomato = emoji.get("tomato")
+
+const failed = "Assertion Failed: ";
+const passed = "Assertion Passed: ";
+const quote = '"';
+const notEqual = " !== ";
+const equal = " === "
 const assertEqual = function (actual, expected) {
 
   if (actual !== expected) {
     if (typeof actual === 'string') {
-      console.log(pizza + pizza + pizza + "Assertion Failed: " + '"' + actual + '"' + " !== " + '"' + expected + '"');
+      console.log(`${pizza}${pizza}${pizza}${failed}${quote}${actual}${quote}${notEqual}${quote}${expected}${quote}`)
     } else if (typeof actual === 'number') {
-      console.log(pizza + pizza + pizaa + "Assertion Failed: " + '"' + actual + '"' + " !== " + '"' + expected + '"');
+      console.log(`${pizza}${pizza}${pizza}${failed}${actual}${notEqual}${expected}`);
     }
   } else {
     if (typeof actual === 'string') {
-      console.log(tomato + tomato + tomato + "Assertion Passed: " + actual + " === " + expected);
+      console.log(`${tomato}${tomato}${tomato}${passed}${quote}${actual}${quote}${equal}${quote}${expected}${quote}`);
     } else if (typeof actual === 'number') {
-      console.log(tomato + tomato + tomato + "Assertion Passed: " + actual + " === " + expected);
+      console.log(`${tomato}${tomato}${tomato}${passed}${actual}${equal}${expected}`);
     }
   }
 };
