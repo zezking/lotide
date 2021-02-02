fdf50e4f762fe58c4ab31337fbc9eee6757e7185
zezking <zhaoenze001@gmail.com>
2021-02-02 21:24:54 +0000

    added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+const assertEqual = function (actual, expected) {
+  console.log(actual, expected)
+  if (actual !== expected) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+// TEST CODE
+
+const eqArrays = function (array1, array2) {
+  console.log(array1)
+  console.log(array2)
+  input = [];
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = array1.length - 1; i > -1; i--) {
+      if (assertEqual(array1[i], array2[i])) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
