d110387e1c6bcbff8b47e1376dd3c32c075b11cd
zezking <zhaoenze001@gmail.com>
2021-02-02 21:51:24 +0000

    update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,33 +1,35 @@
+
+
 const assertEqual = function (actual, expected) {
-  console.log(actual, expected)
   if (actual !== expected) {
     return false;
   } else {
     return true;
   }
 };
-
 // TEST CODE
-
 const eqArrays = function (array1, array2) {
-  console.log(array1)
-  console.log(array2)
   input = [];
   if (array1.length !== array2.length) {
     return false;
   } else {
     for (let i = array1.length - 1; i > -1; i--) {
       if (assertEqual(array1[i], array2[i])) {
-        return true;
+        console.log("Assertion Passed: First Array ", array1, "=== Second Array ", array2)
+        break;
       } else {
-        return false;
+        console.log("Assertion Failed: First Array ", array1, "!== Second Array ", array2)
+        break;
       }
     }
   }
 }
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+// Assertion Failed: "Lighthouse Labs" !== "Bootcamp"
+// 🍅🍅🍅Assertion Passed: 1 === 1
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"])// => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
