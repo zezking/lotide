052821aabcd270010dd69165f2a9b90dccf805c5
zezking <zhaoenze001@gmail.com>
2021-02-06 15:46:14 +0000

    modify countLetters and findkeybyvalue

--- countLetters.js
+++ countLetters.js
@@ -1,43 +1,32 @@
 const failed = "Assertion Failed: ";
 const passed = "Assertion Passed: ";
 const notEqual = " !== ";
-const equal = " === "
+const equal = " === ";
 
 const assertEqual = function (actual, expected) {
-
   if (actual !== expected) {
-
     console.log(`${failed}${actual}${notEqual}${expected}`);
-
   } else {
-
     console.log(`${passed}${actual}${equal}${expected}`);
   }
-}
+};
 
 const countLetters = function (str) {
-
-  let result = {}
+  let result = {};
   for (c of str) {
-    if (c !== ' ') {
-      if (result[c]) {
-        result[c]++
-      } else {
-        result[c] = 1;
-      }
+    if (c !== " ") {
+      result[c] ? result[c]++ : (result[c] = 1);
     }
   }
   return result;
-}
+};
 
 const testStr = "lighthouse in the house";
-const testStr2 = "ddddfffjjooooo"
+const testStr2 = "ddddfffjjooooo";
 
-result1 = countLetters(testStr2);
+result1 = countLetters(testStr);
 
-assertEqual(result1['d'], 4)
-assertEqual(result1['f'], 3)
-assertEqual(result1['j'], 2)
-assertEqual(result1['o'], 5)
-
-
+assertEqual(result1["i"], 2);
+assertEqual(result1["f"], 3);
+assertEqual(result1["j"], 2);
+assertEqual(result1["o"], 5);
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,21 +1,15 @@
 const failed = "Assertion Failed: ";
 const passed = "Assertion Passed: ";
 const notEqual = " !== ";
-const equal = " === "
+const equal = " === ";
 
 const assertEqual = function (actual, expected) {
-
   if (actual !== expected) {
-
     console.log(`${failed}${actual}${notEqual}${expected}`);
-
   } else {
-
     console.log(`${passed}${actual}${equal}${expected}`);
   }
-
-}
-
+};
 
 const findKeyByValue = function (obj, str) {
   for (const prop in obj) {
@@ -24,33 +18,31 @@ const findKeyByValue = function (obj, str) {
     }
   }
   return undefined;
-}
+};
 
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire",
-  drama: "The Breaking Bad"
+  drama: "The Breaking Bad",
 };
 
 const testObj = {
-  a: '1',
-  b: '2',
-  c: '3',
-  d: '5',
-  e: '6'
-}
+  a: "1",
+  b: "2",
+  c: "3",
+  d: "5",
+  e: "6",
+};
 
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
-console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show"))
-
-
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show"));
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
-assertEqual(findKeyByValue(testObj, 2), 'b');
-assertEqual(findKeyByValue(testObj, 3), 'c');
-assertEqual(findKeyByValue(testObj, 4), 'b');
-assertEqual(findKeyByValue(testObj, 5), 'd');
+assertEqual(findKeyByValue(testObj, 2), "b");
+assertEqual(findKeyByValue(testObj, 3), "c");
+assertEqual(findKeyByValue(testObj, 4), "b");
+assertEqual(findKeyByValue(testObj, 5), "d");
 assertEqual(findKeyByValue(testObj, 7), undefined);
